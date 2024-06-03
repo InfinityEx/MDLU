06149244cfbd17499bc8b49f80b1191bd8fcf057
時無_ShiWu <756423078@qq.com>
2024-06-04 00:07:18 +0800

    update wrong argprase(-mini,-update)

--- HashGenerator.py
+++ HashGenerator.py
@@ -5,10 +5,11 @@ import sys
 import json
 import argparse
 from colorama import Fore,init,Back
+import requests as rqs
 
 init()
 # getPath
-def pathWalk(outType):
+def pathWalk(outType,output=''):
     if os.path.exists(os.getcwd()+"/preference.json"):
         # print(os.path.exists(os.getcwd()+"/preference.json"))
         with open(os.getcwd()+"/preference.json",'r',encoding='utf-8') as pf:
@@ -20,26 +21,55 @@ def pathWalk(outType):
                 options['hashlist']=f"{options['path']}{settings['Game'][0]['hashlist']}"
                 # print(os.path.exists(options['data']))
                 if outType=='json':
-                    return print(options)
+                    if output=='':
+                        return print(options)
+                    if output=='real':
+                        return options
                 elif outType=='text':
                     return print(f"path:{options['path']}\ndata:{options['data']}\nhash:{options['hashlist']}")
                 else:
                     exit
             except:
                 print(f'{Fore.LIGHTRED_EX}Fatal Error: Incorrect JSON structure{Fore.RESET}')
+                createPreference()
+                print(f"preference file restored")
+                
     else:
         print(f'{Fore.LIGHTRED_EX}Error: preference file not found{Fore.RESET}')
+        createPreference()
+        print(f"preference file restored")
 
+def createPreference():
+    pfs={
+        "Updater":
+            [{
+                "version":"0.1",
+                "":""
+                }],
+        "Game":
+            [{
+                "currentVersion":None,
+                "previousVersion":None,
+                "path":None,
+                "data":None,
+                "hashlist":None
+            }],
+            "showUpdate":False
+        }
+    with open(f"{os.getcwd()}/preference.json",'w',encoding='utf-8') as pf:
+        json.dump(pfs,pf,indent=2,ensure_ascii=False)
 
 # getHash
 def getHash(path):
     md5=hashlib.md5(open(path,'rb').read()).hexdigest()
     return md5
-    pass
 
 
 # fileWalk
-def interia(path):
+def interia(data,path=''):
+    with open(f"{data}/version.json",'r',encoding='utf-8') as vs:
+        da=json.load(vs)
+        ver=da['version']
     list={
         "data":[]
     }
@@ -56,15 +86,15 @@ def interia(path):
                 "filename":file_name,
                 "path":os.path.join(root,file).replace('\\','/').split(path)[1],
                 "md5":getHash(file_patht),
-                "version":"0.4.5"
+                "version":ver
                 }
-            print("os path join: "+os.path.join(root,file))
-            print("path: "+path)
-            print("file_pathr"+file_pathr)
-            print("file_patht"+file_patht)
-            print(litdetial["path"])
+            # print("os path join: "+os.path.join(root,file))
+            # print("path: "+path)
+            # print("file_pathr"+file_pathr)
+            # print("file_patht"+file_patht)
+            # print(litdetial["path"])
             list["data"].append(litdetial)
-    with open(f"{path}/hash.json",'w',encoding='utf-8') as hf:
+    with open(f"{path}/list.json",'w',encoding='utf-8') as hf:
         json.dump(list,hf,indent=2,ensure_ascii=False)
     pass
 
@@ -82,6 +112,7 @@ if __name__ == '__main__':
     parser.add_argument('-g','-gen',nargs='?',help=f'{Fore.LIGHTCYAN_EX}Generate hash file from game path[need specified],like "hashGen.exe -g "D:/Games/MiniDL/MiniDL_Data"{Fore.MAGENTA}')
     parser.add_argument('--version',help=f'{Fore.LIGHTMAGENTA_EX}Print program version{Fore.RESET}',action="store_true")
     parser.add_argument('-mini',action="store_true")
+    parser.add_argument('-update',action="store_true")
 
 
     # merge parser
@@ -98,10 +129,49 @@ if __name__ == '__main__':
         sys.exit()
 
     if args.mini:
-        print(args)
-        realpath=pathWalk(json)
-        print(realpath,type(realpath))
+        realpath=pathWalk('json','real')
+        # print(realpath)
+        print(f" data_path: {realpath['data']}\n launcher_path: {realpath['path']}\n")
+        interia(realpath['data'],realpath['path'])
+        print(f'{Fore.LIGHTCYAN_EX} JSON file is generated in {Fore.LIGHTGREEN_EX}"{realpath["path"]}"{Fore.RESET}')
         pass
+
+    if args.update:
+        realpath=pathWalk('json','real')
+        path=realpath['path']
+        url="http://127.0.0.1:99/minidl"
+        head="/list.json"
+        response=rqs.get(url+head)
+        if os.path.exists(f"{path}/patch")==False:
+            os.mkdir(f"{path}/patch")
+        if response.status_code==200:
+            with open(f"{path}/patch/list.json",'w',encoding='utf-8') as hf:
+                hf.write(response.text)
+        new={}
+        local={}
+        with open(f"{path}/list.json",'r',encoding='utf-8') as nw:
+            new=json.load(nw)
+        with open(f"{path}/patch/list.json",'r',encoding='utf-8') as lc:
+            local=json.load(lc)
+        for i in range(0,len(new['data'])):
+            for j in range(0,len(local['data'])):  
+                if new['data'][i]['filename']==local['data'][j]['filename']:
+                    if new['data'][i]['md5']!=local['data'][j]['md5']:
+                        res=rqs.get(f"{url}/{new['data'][i]['path']}")
+                        with open(f"{path}/{new['data'][i]['path']}",'wb') as file1:
+                            file1.write(res.content)
+                        local['data'][j].update(new['data'][i])
+                        print(f"{local['data'][j]}, {new['data'][i]}\n")
+                elif new['data'][i]['filename']!=local['data'][j]['filename']:
+                    resa=rqs.get(f"{url}/{new['data'][i]['path']}")
+                    with open(f"{path}/{new['data'][i]['path']}",'wb') as file2:
+                        file2.write(resa.content)
+                    local['data'].append(new['data'][i])
+                    print(f"{new['data'][i]}, {local['data']}\n")
+                
+        # print(local["data"][0])
+        pass
+
 
     # get Hash
     if args.g!=None:
@@ -112,6 +182,6 @@ if __name__ == '__main__':
             print(f"\n{Fore.LIGHTRED_EX} Warning: path is unavailable\n{Fore.RESET}")
             parser.print_help()
     
-    if args.p==None and args.g==None and args.mini==False:
+    if args.p==None and args.g==None and args.mini==False and args.update==False:
         parser.print_help()
     pass
